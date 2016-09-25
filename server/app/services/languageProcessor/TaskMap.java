e934cd8b5908a537485d78d60b9941ae870bedc3
Sabina Pokhrel <sabinaa.pokhrel@gmail.com>
2016-09-25 16:02:05 +1000

    Code cleaning

--- server/app/services/languageProcessor/TaskMap.java
+++ server/app/services/languageProcessor/TaskMap.java
@@ -1,26 +1,35 @@
 package services.languageProcessor;
 import java.lang.reflect.*;
-//import httpclient;
 
 class TaskMap{
 
-    /*
-    This method calls appropriate method based on the parameters passed
-     */
+  /**
+   * This method calls appropriate method on run time based on the
+   * parameters (methodName and argName) passed and returns a value
+   * returned by that method.
+   * @param methodName
+   * @param argName
+   * @return
+   * @throws NoSuchMethodException
+   * @throws InvocationTargetException
+   * @throws IllegalAccessException
+   */
     public static String questionMapping(String methodName, String argName) throws NoSuchMethodException,
             InvocationTargetException,IllegalAccessException{
 
         TaskMap tm = new TaskMap();
+
         //call the method at runtime according to the argument "methodName"
         Method m = TaskMap.class.getMethod(methodName, String.class);
         String returnVal = (String) m.invoke(tm, argName);
         return returnVal;
     }
 
-
-    /*
-    This method requests ticket info and returns it to the calling method
-     */
+  /**
+   * This method requests ticket info and returns it to the calling method
+   * @param ticket
+   * @return
+   */
     public String description_of_ticket(String ticket){
         String answer = "Description of the ticket " + ticket;
         System.out.println(answer);
@@ -28,13 +37,14 @@ class TaskMap{
         return answer;
     }
 
-    /*
-    This method requests assignee of ticket and returns it to the calling method
-     */
-    public String assignee_of_ticket(String ticket){
+  /**
+   * This method requests assignee of ticket and returns it to the calling method
+   * @param ticket
+   * @return
+   */
+  public String assignee_of_ticket(String ticket){
         String answer = "The person working on "+ ticket +" is ";
         System.out.println(answer);
-        //String assignee = JIRAconnector.getAssignee(ticket);
         return answer;
     }
 }
