c1a3046e40f041d49209bfde952937bdc65b7e59
王栩 <wangxu@wangxudeMacBook-Pro.local>
2016-09-28 12:37:35 +1000

    add JIRA connector.

--- server/app/services/httpclient/JIRAconnector.java
+++ server/app/services/httpclient/JIRAconnector.java
@@ -3,61 +3,36 @@ package services.httpclient;
 import java.io.*;
 import java.net.*;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.Gson;
+import javax.inject.Inject;
+
+import play.mvc.*;
+import play.libs.ws.*;
+import java.util.concurrent.CompletionStage;
 
 
 
-public class JIRAconnector{
-
-    public static String getHTML(String urlToRead) throws Exception {
-        StringBuilder result = new StringBuilder();
-        String userPassword = "xuwang" + ":" + "woshishui";
-        String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
-        Gson gson = new Gson();
 
 
-        URL url = new URL(urlToRead);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+public class JIRAconnector extends Controller{
+
+    @Inject WSClient ws;
+
+    public String temp(){
 
 
-        conn.setRequestProperty("Authorization", "Basic " + encoding);
-        conn.setRequestMethod("GET");
-
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-        }
-
-        //System.out.println(result);
-        rd.close();
-
-        return result.toString();
+        WSRequest complexRequest = ws.url("jira.agiledigital.com.au/rest/api/latest/issue/POET-3").setAuth("xuwang", "woshishui", WSAuthScheme.BASIC);
+        CompletionStage<WSResponse> responsePromise = complexRequest.get();
+        //System.out.println('1');
+        //System.out.println(responsePromise);
+        String ans  = responsePromise.toString();
+        return ans;
     }
 
-    public static String getDescription(String id)throws Exception{
-        Gson gson = new Gson();
-        String result = getHTML("https://jira.agiledigital.com.au/rest/api/latest/issue/" + id);
-        Tickets tickets = gson.fromJson(result,Tickets.class);
 
-        String description = tickets.getFields().getIssuetype().getDescription();
-
-        return description;
-    }
-
-    public static String getAssignee(String id) throws  Exception{
-        Gson gson = new Gson();
-        String result = getHTML("https://jira.agiledigital.com.au/rest/api/latest/issue/" + id);
-        Tickets tickets = gson.fromJson(result,Tickets.class);
-
-        String assignee = tickets.getFields().getAssignee().getName();
-
-        return assignee;
-
-    }
+    //public static void main(String[] args) {
+      //  JIRAconnector jirAconnector = new JIRAconnector();
+        //jirAconnector.temp();
+    //}
 
 
 }
