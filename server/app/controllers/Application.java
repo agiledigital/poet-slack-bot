16612fc622511c65bbd076acb52ed17b0470c116
Dongxu LI <Dongxu LI>
2016-09-29 12:01:48 +1000

    try Async Web service.

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -1,28 +1,50 @@
 package controllers;
 
+import play.libs.ws.WSAuthScheme;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
 import play.mvc.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 
+import javax.inject.Inject;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.CompletionStage;
 
 public class Application extends Controller {
+
+  @Inject
+  WSClient ws;
 
   public Result index() {
     return ok("Hi!");
   }
 
-  public Result QA(String question) throws IOException,
+  public CompletionStage<Result> QA(String question) throws IOException,
     ClassNotFoundException, NoSuchMethodException,
     InvocationTargetException, IllegalAccessException {
 
     String answer = services.languageProcessor.Processor.processQuestion(question);
 
+    WSRequest request = ws.url("https://jira.agiledigital.com.au/rest/api/latest/issue/POET-3");
+    WSRequest complexRequest = request.setAuth("xuwang", "woshishui", WSAuthScheme.BASIC);
+
+    CompletionStage<WSResponse> responsePromise = complexRequest.get();
+    CompletionStage<Result> promiseOfResult = responsePromise.thenApply(pi ->
+      ok("PI value computed: " + pi.getBody())
+    );
+
+
+/%
+    %/
+
     // parse the JSON as a JsonNode
     JsonNode json = Json.parse("{\"answer\":\"" +answer+ "\"}");
 
-    return ok(json);
+    return promiseOfResult;
+    //return ok(json);
   }
 
 }
