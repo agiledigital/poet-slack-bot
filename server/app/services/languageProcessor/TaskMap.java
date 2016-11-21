624871fc5393e4fa76da677d9b3c7461e103f9e9
Sabina Pokhrel <sabinaa.pokhrel@gmail.com>
2016-11-21 23:21:57 +1100

    Methods added for handling additional questions

--- server/app/services/languageProcessor/TaskMap.java
+++ server/app/services/languageProcessor/TaskMap.java
@@ -15,6 +15,26 @@ import java.util.List;
 public class TaskMap {
 
   /**
+   * This method gets the topScoring intent and identified entities
+   * from LUIS API and calls methods to perform the required action.
+   * @param responseBody
+   * @return
+   */
+  public static JsonNode questionMapping(JsonNode responseBody) {
+    System.out.print((responseBody.toString()));
+    String topScoringIntent = responseBody.get("topScoringIntent").get("intent").toString();
+    List entities = responseBody.get("entities").findValues("entity");
+    List entityTypes = responseBody.get("entities").findValues("type");
+
+    IntentEntity intentEntity = new IntentEntity();
+    intentEntity.intent = topScoringIntent.toString().replace("\"", "");
+    intentEntity.entityType = entityTypes.get(0).toString().replace("\"", "");
+    intentEntity.entityName = entities.get(0).toString().replace("\"", "").replace(" ","");
+
+    return Json.toJson((intentEntity));
+  }
+
+  /**
    * This method calls appropriate method on run time based on the
    * parameters (methodName and argName) passed and returns a value
    * returned by that method.
@@ -22,7 +42,6 @@ public class TaskMap {
    * @param issueKey
    * @return
    */
-
   public static JsonNode questionMapping(String methodName, String issueKey, JsonNode responseBody) {
 
     TaskMap taskMap = new TaskMap();
@@ -51,7 +70,7 @@ public class TaskMap {
    * @param issueKey
    * @return
    */
-  public JsonNode getTicketDescription(String issueKey, JsonNode responseBody) {
+  public JsonNode IssuetDescription(String issueKey, JsonNode responseBody) {
     if (Extractor.extractString(responseBody, "description").equals("[\"Issue Does Not Exist\"]")) {
       return parseToJson("fail", "Cannot find issue");
     } else {
@@ -68,11 +87,107 @@ public class TaskMap {
    * @param issueKey
    * @return
    */
-  public JsonNode getTicketAssignee(String issueKey, JsonNode responseBody) {
+  public JsonNode IssueAssignee(String issueKey, JsonNode responseBody) {
     if (Extractor.extractString(responseBody, "assignee").equals("[\"Issue Does Not Exist\"]")) {
       return parseToJson("fail", "Cannot find issue");
     } else {
       String answer = Extractor.extractString(responseBody, "assignee") + " is working on " + issueKey + ".";
+      System.out.println(answer);
+      return parseToJson("success", answer);
+    }
+  }
+
+  /**
+   * This method requests breif description of issue and returns it to the calling method
+   *
+   * @param issueKey
+   * @return
+   */
+  public JsonNode IssueBrief(String issueKey, JsonNode responseBody) {
+    if (Extractor.extractString(responseBody, "assignee").equals("[\"Issue Does Not Exist\"]")) {
+      return parseToJson("fail", "Cannot find issue");
+    } else {
+      String answer = Extractor.extractString(responseBody, "assignee") + " is working on " + issueKey + ".";
+      System.out.println(answer);
+      return parseToJson("success", answer);
+    }
+  }
+
+  /**
+   * This method requests status of an issue and returns it to the calling method
+   *
+   * @param issueKey
+   * @return
+   */
+  public JsonNode IssueStatus(String issueKey, JsonNode responseBody) {
+    if (Extractor.extractString(responseBody, "status").equals("[\"Issue Does Not Exist\"]")) {
+      return parseToJson("fail", "Cannot find issue");
+    } else {
+      String answer = "Status of "+issueKey;//Extractor.extractString(responseBody, "status") + " is working on " + issueKey + ".";
+      System.out.println(answer);
+      return parseToJson("success", answer);
+    }
+  }
+
+  /**
+   * This method requests issues that are in progress and returns it to the calling method
+   *
+   * @param issueKey
+   * @return
+   */
+  public JsonNode InProgressIssues(String issueKey, JsonNode responseBody) {
+    if (Extractor.extractString(responseBody, "status").equals("[\"Issue Does Not Exist\"]")) {
+      return parseToJson("fail", "Cannot find issue");
+    } else {
+      String answer = "Status of "+issueKey;//Extractor.extractString(responseBody, "status") + " is working on " + issueKey + ".";
+      System.out.println(answer);
+      return parseToJson("success", answer);
+    }
+  }
+
+  /**
+   * This method requests issues that are completed and returns it to the calling method
+   *
+   * @param issueKey
+   * @return
+   */
+  public JsonNode CompletedIssues(String issueKey, JsonNode responseBody) {
+    if (Extractor.extractString(responseBody, "status").equals("[\"Issue Does Not Exist\"]")) {
+      return parseToJson("fail", "Cannot find issue");
+    } else {
+      String answer = "Status of "+issueKey;//Extractor.extractString(responseBody, "status") + " is working on " + issueKey + ".";
+      System.out.println(answer);
+      return parseToJson("success", answer);
+    }
+  }
+
+  /**
+   * This method requests issues that are stalled and returns it to the calling method
+   *
+   * @param issueKey
+   * @return
+   */
+  public JsonNode StalledIssues(String issueKey, JsonNode responseBody) {
+    if (Extractor.extractString(responseBody, "status").equals("[\"Issue Does Not Exist\"]")) {
+      return parseToJson("fail", "Cannot find issue");
+    } else {
+      String answer = "Status of "+issueKey;//Extractor.extractString(responseBody, "status") + " is working on " + issueKey + ".";
+      System.out.println(answer);
+      return parseToJson("success", answer);
+    }
+  }
+
+  /**
+   * This method sets the project for a channel
+   *
+   * @param issueKey
+   * @return
+   */
+  public JsonNode SetProject(String issueKey, JsonNode responseBody) {
+    if (Extractor.extractString(responseBody, "status").equals("[\"Issue Does Not Exist\"]")) {
+      return parseToJson("fail", "Cannot find issue");
+    } else {
+      String answer = "Status of "+issueKey;//Extractor.extractString(responseBody, "status") + " is working on " + issueKey + ".";
       System.out.println(answer);
       return parseToJson("success", answer);
     }
@@ -94,19 +209,5 @@ public class TaskMap {
 
     return answer;
 
-  }
-
-  public static JsonNode questionMapping(JsonNode responseBody) {
-    System.out.print((responseBody.toString()));
-    String topScoringIntent = responseBody.get("topScoringIntent").get("intent").toString();
-    List entities = responseBody.get("entities").findValues("entity");
-    List entityTypes = responseBody.get("entities").findValues("type");
-
-    IntentEntity intentEntity = new IntentEntity();
-    intentEntity.intent = topScoringIntent.toString().replace("\"", "");
-    intentEntity.entityType = entityTypes.get(0).toString().replace("\"", "");
-    intentEntity.entityName = entities.get(0).toString().replace("\"", "").replace(" ","");
-
-    return Json.toJson((intentEntity));
   }
 }
