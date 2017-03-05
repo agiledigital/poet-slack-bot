9ea194f0334d04c3082042b026583de941a07e0b
dongxuDX <Dongxu_Li@outlook.com>
2017-03-05 11:46:43 +1100

    function to hyperlink ticket number in the description.

--- server/app/services/Utils.java
+++ server/app/services/Utils.java
@@ -1,11 +1,13 @@
 package services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.Configuration;
+import play.api.Play;
 
 import java.io.*;
 
 public class Utils {
-
   public static JiraInfo getJiraInfo(Configuration configuration){
    return new JiraInfo(configuration.getString("jira.username"), configuration.getString("jira.password"));
   }
@@ -21,6 +23,27 @@ public class Utils {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public static JsonNode hyperlinkTicketNo(JsonNode responseBody){
+    Configuration configuration = Play.current().injector().instanceOf(Configuration.class);
+
+    String jiraIssueName = configuration.getString("jira.issueName");
+    String pattern = "(" + jiraIssueName + "-\\d+)";
+    String issueDescription = responseBody.get("fields").get("description").toString();
+    String issueUrl = configuration.getString("jira.baseUrl") + "/browse/";
+    String hyperlink = "<" + issueUrl + "$1|$1>";
+
+    ObjectNode responseObj;
+    ObjectNode objectNode = (ObjectNode) responseBody.get("fields");
+    issueDescription = issueDescription.replaceAll(pattern, hyperlink);
+
+    objectNode.put("description", issueDescription);
+
+    responseObj = (ObjectNode) responseBody;
+    responseObj.set("fields", objectNode);
+
+    return responseBody;
   }
 }
 
